e464393dcec39aa450e0d4e6c0ac6ccbf6bc9d8a
Corey Psoinos <corey.psoinos@contentsquare.com>
2024-12-29 11:23:09 -0500

    fetch handler for DO since rpc isn't supported locally

--- apps/web/src/routes/register/+page.server.ts
+++ apps/web/src/routes/register/+page.server.ts
@@ -9,6 +9,8 @@ import {
 	sendVerificationEmail,
 	setEmailVerificationRequestCookie
 } from '$lib/server/email-verification';
+import { Bindings } from '$lib/server/bindings';
+import type { RefillingTokenBucket } from '@ficus/rate-limiter/src';
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.user) {
@@ -17,8 +19,51 @@ export const load: PageServerLoad = async (event) => {
 	return {};
 };
 
+const REFILL_RATE = 1;
+const CAPACITY = 3;
+const UPDATE_MS = 10000;
+
+async function getIpBucket(ip: string | null): Promise<DurableObjectStub<RefillingTokenBucket>> {
+	const name = `${ip}:register`;
+	const id = Bindings.env.REFILLING_TOKEN_BUCKET.idFromName(name);
+	const stub = Bindings.env.REFILLING_TOKEN_BUCKET.get(id);
+	// stub.setParams({ refillRate: REFILL_RATE, capacity: CAPACITY, updateMs: UPDATE_MS });
+	await stub.fetch('https://ficus-rate-limiter.local/set-params', {
+		method: 'POST',
+		body: JSON.stringify({ refillRate: REFILL_RATE, capacity: CAPACITY, updateMs: UPDATE_MS })
+	});
+	return stub;
+}
+
+async function checkIpRateLimit(
+	ip: string | null
+): Promise<{ allowed: boolean; remainingTokens: number; nextRefillTime?: number }> {
+	const ipBucket = await getIpBucket(ip);
+	// return ipBucket.consume(1);
+	const resp = await ipBucket.fetch('https://ficus-rate-limiter.local/consume', {
+		method: 'POST',
+		body: JSON.stringify({ tokens: 1 })
+	});
+	return resp.json();
+}
+
 export const actions: Actions = {
 	register: async (event) => {
+		const clientIP = event.getClientAddress();
+		const { allowed, remainingTokens, nextRefillTime } = await checkIpRateLimit(clientIP);
+		if (!allowed) {
+			console.warn(`Rate limit exceeded for IP: ${clientIP}`, {
+				route: '/register',
+				clientIP,
+				remainingTokens,
+				...(nextRefillTime && {
+					nextRefillTime: new Date(nextRefillTime).toISOString(),
+					nextRefillIn: nextRefillTime - Date.now()
+				})
+			});
+			return fail(429, { message: 'Rate limit exceeded' });
+		}
+
 		const formData = await event.request.formData();
 		const email = formData.get('email') as string | undefined;
 		const password = formData.get('password') as string | undefined;
--- packages/rate-limiter/src/RefillingTokenBucket.ts
+++ packages/rate-limiter/src/RefillingTokenBucket.ts
@@ -1,7 +1,9 @@
 import { DurableObject } from 'cloudflare:workers';
+import { Hono } from 'hono';
 
 export class RefillingTokenBucket extends DurableObject {
   storage: DurableObjectStorage;
+  app = new Hono<{ Bindings: Env }>();
 
   refillRate: number | null = null; // Tokens added per second
   capacity: number | null = null; // Maximum number of tokens
@@ -10,9 +12,26 @@ export class RefillingTokenBucket extends DurableObject {
   constructor(ctx: DurableObjectState, env: Env) {
     super(ctx, env);
     this.storage = ctx.storage;
+
+    this.app.post('/set-params', async (c) => {
+      const { refillRate, capacity, updateMs } = await c.req.json();
+      this.setParams({ refillRate, capacity, updateMs });
+      return c.status(200);
+    });
+
+    this.app.post('/consume', async (c) => {
+      const { tokens } = await c.req.json();
+      const response = await this.consume(tokens);
+      return c.json(response);
+    });
   }
 
-  setParams({
+  // Fetch handler since RPC is not yet supported between multiple `wrangler dev` sessions
+  async fetch(request: Request) {
+    return this.app.fetch(request);
+  }
+
+  async setParams({
     refillRate,
     capacity,
     updateMs,
@@ -20,16 +39,34 @@ export class RefillingTokenBucket extends DurableObject {
     refillRate: number;
     capacity: number;
     updateMs: number;
-  }): void {
+  }): Promise<void> {
+    await Promise.all([
+      this.storage.put('refillRate', refillRate),
+      this.storage.put('capacity', capacity),
+      this.storage.put('updateMs', updateMs),
+    ]);
     this.refillRate = refillRate;
     this.capacity = capacity;
     this.updateMs = updateMs;
   }
 
+  private async getParams(): Promise<void> {
+    const [refillRate, capacity, updateMs] = await Promise.all([
+      this.storage.get<number>('refillRate'),
+      this.storage.get<number>('capacity'),
+      this.storage.get<number>('updateMs'),
+    ]);
+    this.refillRate = refillRate ?? null;
+    this.capacity = capacity ?? null;
+    this.updateMs = updateMs ?? null;
+  }
+
   // RPC method to consume tokens
-  async consume(
-    tokens = 1
-  ): Promise<{ allowed: boolean; remainingTokens: number }> {
+  async consume(tokens = 1): Promise<{
+    allowed: boolean;
+    remainingTokens: number;
+    nextRefillTime?: number;
+  }> {
     if (!this.refillRate || !this.capacity || !this.updateMs) {
       throw new Error('Params not set!');
     }
@@ -46,7 +83,19 @@ export class RefillingTokenBucket extends DurableObject {
     }
 
     await this.checkAndSetAlarm();
-    return { allowed: false, remainingTokens };
+    return {
+      allowed: false,
+      remainingTokens,
+      nextRefillTime: await this.getNextRefillTime(),
+    };
+  }
+
+  private async getNextRefillTime(): Promise<number> {
+    const now = Date.now();
+    const lastRefillTime =
+      (await this.storage.get<number>('lastRefillTime')) ?? now;
+    const elapsedMilliseconds = now - lastRefillTime;
+    return lastRefillTime + elapsedMilliseconds;
   }
 
   // Refills tokens based on elapsed time
@@ -69,12 +118,14 @@ export class RefillingTokenBucket extends DurableObject {
     if (refillTokens > 0) {
       remainingTokens = Math.min(this.capacity, remainingTokens + refillTokens);
       await this.storage.put('remainingTokens', remainingTokens);
-      await this.storage.put('lastRefillTime', now);
     }
+    await this.storage.put('lastRefillTime', now);
   }
 
   // Alarm handler for periodic token refill
   async alarm(): Promise<void> {
+    // get params from storage
+    await this.getParams();
     await this.refillTokens();
     await this.checkAndSetAlarm();
   }
