b8e90c02ec0a57828fbcf1f0c5298b7e12c67e84
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-10 17:16:15 -0500

    add sidebar and begin adding nav components

---
+++ apps/web/src/lib/components/app-header.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Button } from '$lib/components/ui/button';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import SidebarTrigger from './ui/sidebar/sidebar-trigger.svelte';
+</script>
+
+<header class="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
+	<div class="flex items-center gap-2">
+		<SidebarTrigger class="-ml-1" />
+		<Separator orientation="vertical" class="mr-2 h-4" />
+		<Breadcrumb.Root>
+			<Breadcrumb.List>
+				<Breadcrumb.Item class="hidden md:block">
+					<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+				</Breadcrumb.Item>
+				<Breadcrumb.Separator class="hidden md:block" />
+				<Breadcrumb.Item>
+					<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+				</Breadcrumb.Item>
+			</Breadcrumb.List>
+		</Breadcrumb.Root>
+	</div>
+
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger>
+			{#snippet child({ props })}
+				<Button {...props} variant="outline" size="icon" class="overflow-hidden rounded-full">
+					<img
+						src="/images/placeholder-user.jpg"
+						width={36}
+						height={36}
+						alt="Avatar"
+						class="overflow-hidden rounded-full"
+					/>
+				</Button>
+			{/snippet}
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content align="end">
+			<DropdownMenu.Label>My Account</DropdownMenu.Label>
+			<DropdownMenu.Separator />
+			<DropdownMenu.Item>
+				{#snippet child({ props })}
+					<a href="/settings" {...props}>Settings</a>
+				{/snippet}
+			</DropdownMenu.Item>
+			<DropdownMenu.Item>Support</DropdownMenu.Item>
+			<DropdownMenu.Separator />
+			<DropdownMenu.Item>
+				{#snippet child({ props })}
+					<form method="post" action="/?/logout" use:enhance {...props}>
+						<button>Sign out</button>
+					</form>
+				{/snippet}
+			</DropdownMenu.Item>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+</header>
