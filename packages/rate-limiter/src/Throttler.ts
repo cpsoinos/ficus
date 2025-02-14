f3b95a1802e04ce796a7155b535e3079b5f7237d
Corey Psoinos <corey@anderapps.com>
2025-02-14 11:49:20 -0500

    chore: husky & precommit hooks (#16)
    * setup husky and lint-staged
    
    * sort package.json files, and use prettier from workspace root
    
    * consolidate .gitignores
    
    * format, use unstable_config_lookup_from_file for eslint
    
    * ensure build folders in subfolders are gitignored
    
    * extend base tsconfig

--- packages/rate-limiter/src/Throttler.ts
+++ packages/rate-limiter/src/Throttler.ts
@@ -2,86 +2,77 @@ import { DurableObject } from 'cloudflare:workers';
 import { Hono } from '@hono/hono';
 
 export class Throttler extends DurableObject {
-  storage: DurableObjectStorage;
-  app = new Hono<{ Bindings: Env }>();
+	storage: DurableObjectStorage;
+	app = new Hono<{ Bindings: Env }>();
 
-  timeoutSeconds: number[] | null = null;
+	timeoutSeconds: number[] | null = null;
 
-  constructor(ctx: DurableObjectState, env: Env) {
-    super(ctx, env);
-    this.storage = ctx.storage;
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		this.storage = ctx.storage;
 
-    this.app.post('/set-params', async (c) => {
-      const { timeoutSeconds } = await c.req.json();
-      await this.setParams({ timeoutSeconds });
-      return c.status(200);
-    });
+		this.app.post('/set-params', async (c) => {
+			const { timeoutSeconds } = await c.req.json();
+			await this.setParams({ timeoutSeconds });
+			return c.status(200);
+		});
 
-    this.app.post('/consume', async (c) => {
-      const response = await this.consume();
-      return c.json(response);
-    });
+		this.app.post('/consume', async (c) => {
+			const response = await this.consume();
+			return c.json(response);
+		});
 
-    this.app.post('/reset', async (c) => {
-      await this.reset();
-      return c.status(200);
-    });
-  }
+		this.app.post('/reset', async (c) => {
+			await this.reset();
+			return c.status(200);
+		});
+	}
 
-  override fetch(request: Request) {
-    return this.app.fetch(request);
-  }
+	override fetch(request: Request) {
+		return this.app.fetch(request);
+	}
 
-  async setParams({
-    timeoutSeconds,
-  }: {
-    timeoutSeconds: number[];
-  }): Promise<void> {
-    await this.storage.put('timeoutSeconds', timeoutSeconds);
-    this.timeoutSeconds = timeoutSeconds;
-  }
+	async setParams({ timeoutSeconds }: { timeoutSeconds: number[] }): Promise<void> {
+		await this.storage.put('timeoutSeconds', timeoutSeconds);
+		this.timeoutSeconds = timeoutSeconds;
+	}
 
-  private async getParams(): Promise<void> {
-    if (this.timeoutSeconds) {
-      return;
-    }
-    const timeoutSeconds = await this.storage.get<number[]>('timeoutSeconds');
-    this.timeoutSeconds = timeoutSeconds ?? null;
-  }
+	private async getParams(): Promise<void> {
+		if (this.timeoutSeconds) {
+			return;
+		}
+		const timeoutSeconds = await this.storage.get<number[]>('timeoutSeconds');
+		this.timeoutSeconds = timeoutSeconds ?? null;
+	}
 
-  async consume(): Promise<boolean> {
-    await this.getParams();
-    let counter =
-      (await this.storage.get<ThrottlingCounter>('counter')) ?? null;
-    const now = Date.now();
-    if (counter === null) {
-      counter = {
-        timeout: 0,
-        updatedAt: now,
-      };
-      await this.storage.put('counter', counter);
-      return true;
-    }
-    const allowed =
-      now - counter.updatedAt >= this.timeoutSeconds![counter.timeout] * 1000;
-    if (!allowed) {
-      return false;
-    }
-    counter.updatedAt = now;
-    counter.timeout = Math.min(
-      counter.timeout + 1,
-      this.timeoutSeconds!.length - 1
-    );
-    await this.storage.put('counter', counter);
-    return true;
-  }
+	async consume(): Promise<boolean> {
+		await this.getParams();
+		let counter = (await this.storage.get<ThrottlingCounter>('counter')) ?? null;
+		const now = Date.now();
+		if (counter === null) {
+			counter = {
+				timeout: 0,
+				updatedAt: now
+			};
+			await this.storage.put('counter', counter);
+			return true;
+		}
+		const allowed = now - counter.updatedAt >= this.timeoutSeconds![counter.timeout] * 1000;
+		if (!allowed) {
+			return false;
+		}
+		counter.updatedAt = now;
+		counter.timeout = Math.min(counter.timeout + 1, this.timeoutSeconds!.length - 1);
+		await this.storage.put('counter', counter);
+		return true;
+	}
 
-  async reset() {
-    await this.storage.delete('counter');
-  }
+	async reset() {
+		await this.storage.delete('counter');
+	}
 }
 
 export interface ThrottlingCounter {
-  timeout: number;
-  updatedAt: number;
+	timeout: number;
+	updatedAt: number;
 }
