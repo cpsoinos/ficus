da8650f48d17b780eea1a76825ac8cad193cdf94
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-15 16:13:28 -0500

    delete old commented route protection in base load fn

--- apps/web/src/routes/(protected)/+page.server.ts
+++ apps/web/src/routes/(protected)/+page.server.ts
@@ -3,18 +3,6 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	// if (event.locals.session === null || event.locals.user === null) {
-	// 	return redirect(302, '/login');
-	// }
-	// if (!event.locals.user.emailVerified) {
-	// 	return redirect(302, '/verify-email');
-	// }
-	// if (!event.locals.user.registered2FA) {
-	// 	return redirect(302, '/2fa/setup');
-	// }
-	// if (!event.locals.session.twoFactorVerified) {
-	// 	return redirect(302, '/2fa');
-	// }
 	return {
 		user: event.locals.user!
 	};
