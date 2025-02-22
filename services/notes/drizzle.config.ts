6e90a8ee8a820efa36a31fa2f6429b2f1e1aaf4b
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-22 15:54:26 -0500

    use op cli to load secrets

--- services/notes/drizzle.config.ts
+++ services/notes/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 // @ts-expect-error this file is used for local development, which uses node, so process is valid. Not worth adding a whole new tsconfig just for this.
-const { DATABASE_URL, CF_D1_DB_ID, CF_D1_TOKEN, CF_ACCOUNT_ID } = process.env;
+const { DATABASE_URL, CF_D1_DB_ID, CF_D1_TOKEN, CLOUDFLARE_ACCOUNT_ID } = process.env;
 
 export default defineConfig({
 	schema: './src/db/schema.ts',
@@ -17,7 +17,7 @@ export default defineConfig({
 				dbCredentials: {
 					databaseId: CF_D1_DB_ID!,
 					token: CF_D1_TOKEN!,
-					accountId: CF_ACCOUNT_ID!
+					accountId: CLOUDFLARE_ACCOUNT_ID!
 				},
 				tablesFilter: ['!_cf_KV']
 			})
