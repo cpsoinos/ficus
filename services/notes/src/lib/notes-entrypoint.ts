ea5ce032b19491d3dda3a6e2b2c0100ff26b6efb
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-23 11:49:11 -0500

    feat(notes): better typing applied to base model

--- services/notes/src/lib/notes-entrypoint.ts
+++ services/notes/src/lib/notes-entrypoint.ts
@@ -1,6 +1,6 @@
 import { BaseModel } from './base-model';
 
-export class NotesEntrypoint extends BaseModel {
+export class NotesEntrypoint extends BaseModel<'notes'> {
 	constructor(ctx: ExecutionContext, env: Env) {
 		super(ctx, env, 'notes');
 	}
