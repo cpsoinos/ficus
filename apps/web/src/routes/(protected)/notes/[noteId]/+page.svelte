75ec0e1f8587171697f73523956a4e1423c04ce9
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-23 18:11:09 -0500

    cleanup

--- apps/web/src/routes/(protected)/notes/[noteId]/+page.svelte
+++ apps/web/src/routes/(protected)/notes/[noteId]/+page.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import type { PageProps } from './$types';
-	// import { page } from '$app/state';
 
 	let { data }: PageProps = $props();
 	const { note } = data;
-
-	// const { noteId } = page.params;
-
-	// 	const regExpFilename = /filename="(?<filename>.*)"/;
-
-	// 	// TODO: get filename from page data, then implement download without using object url intermediary
-
-	// 	async function download() {
-	// 		const res = await fetch(`/notes/${id}/attachment`);
-	// 		const blob = await res.blob();
-	// 		const filename: string | null =
-	// 			regExpFilename.exec(res.headers.get('Content-Disposition')!)?.groups?.filename ?? null;
-	// 		const url = URL.createObjectURL(blob);
-	// 		const a = document.createElement('a');
-	// 		a.href = url;
-	// 		a.download = filename ?? 'attachment';
-	// 		a.click();
-	// 		URL.revokeObjectURL(url);
-	// 	}
-	//
 </script>
 
 {#if !note}
@@ -44,5 +23,3 @@
 		{/each}
 	{/if}
 {/if}
-<!-- <Button onclick={download}>Download attachment</Button> -->
-<!-- <Button href={`/notes/${id}/attachment`} download={attachment.filename}>Fetch attachment</Button> -->
