187a248babb87092782fc3e432db513c17008960
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-25 20:30:04 -0500

    use carta for markdown editor

--- apps/web/src/lib/components/note-form.svelte
+++ apps/web/src/lib/components/note-form.svelte
@@ -1,69 +1,41 @@
 <script lang="ts">
-	import 'easymde/dist/easymde.min.css';
-	import 'github-markdown-css/github-markdown-light.css';
-	import * as Tabs from './ui/tabs/index.js';
-	import { marked } from 'marked';
-	import EasyMDE from 'easymde';
-	import { onMount } from 'svelte';
+	import { Carta, MarkdownEditor } from 'carta-md';
+	import { code } from '@cartamd/plugin-code';
+	import { attachment } from '@cartamd/plugin-attachment';
+	import { slash } from '@cartamd/plugin-slash';
+	import 'carta-md/default.css';
+	import '@cartamd/plugin-code/default.css';
+	import '@cartamd/plugin-attachment/default.css';
+	import '@cartamd/plugin-slash/default.css';
+	import DOMPurify from 'isomorphic-dompurify';
 
-	let textarea: HTMLTextAreaElement;
-
-	let editor = $state<EasyMDE>();
 	let markdown = $state('');
 
-	// let attachments = $state<File[]>([]);
+	let attachments = $state<File[]>([]);
 
-	onMount(() => {
-		editor = new EasyMDE({
-			element: textarea,
-			spellChecker: false,
-			toolbar: [
-				'bold',
-				'italic',
-				'heading',
-				'|',
-				'quote',
-				'code',
-				'link',
-				'image',
-				'ordered-list',
-				'unordered-list',
-				'|',
-				'guide'
-			]
-			// uploadImage: true
-			// imageUploadFunction(file, onSuccess, onError) {
-			// 	attachments.push(file);
-			// 	const previewUrl = URL.createObjectURL(file);
-			// 	console.log('previewUrl:', previewUrl);
-			// 	onSuccess(previewUrl);
-			// 	onError('Failed to preview image');
-			// 	// URL.revokeObjectURL(previewUrl);
-			// }
-			// imageMaxSize: 1024 * 1024 * 10,
-			// imagesPreviewHandler: function (data, container) {
-			//   container.innerHTML = '';
-			//   const image = document.createElement('img');
-			//   image.src = data.url;
-			//   container.appendChild(image);
-			// },
-		});
-		editor.codemirror.on('change', () => {
-			markdown = editor!.value();
-		});
+	const carta = new Carta({
+		sanitizer: DOMPurify.sanitize,
+		extensions: [
+			code(),
+			attachment({
+				upload: async (file) => {
+					attachments.push(file);
+					return URL.createObjectURL(file);
+				}
+			}),
+			slash()
+		]
 	});
 </script>
 
-<Tabs.Root value="editor" class="w-full">
-	<Tabs.List>
-		<Tabs.Trigger value="editor">Edit</Tabs.Trigger>
-		<Tabs.Trigger value="preview">Preview</Tabs.Trigger>
-	</Tabs.List>
-	<Tabs.Content value="editor">
-		<textarea bind:this={textarea} class="hidden"></textarea>
-	</Tabs.Content>
-	<Tabs.Content value="preview">
-		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-		<div class="markdown-body p-4">{@html marked(markdown)}</div>
-	</Tabs.Content>
-</Tabs.Root>
+<MarkdownEditor {carta} bind:value={markdown} />
+
+<style lang="postcss">
+	:global(.carta-toolbar) {
+		height: 2.5rem !important;
+	}
+
+	:global(.carta-toolbar-left button) {
+		padding-top: 0.5rem;
+	}
+</style>
