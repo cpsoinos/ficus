88975f68661ac71d113b8785cbb1a1eb510b07b4
Corey Psoinos <coreypsoinos@gmail.com>
2025-03-07 18:04:30 -0500

    update @typescript-eslint/no-unused-vars rule

--- packages/eslint-config/src/configs/base.mjs
+++ packages/eslint-config/src/configs/base.mjs
@@ -20,5 +20,21 @@ export const config = tsConfig(
 				...globals.node
 			}
 		}
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true
+				}
+			]
+		}
 	}
 );
