637ef440e8ad917e9b7ab41b5006b1993c80be58
Corey Psoinos <coreypsoinos@gmail.com>
2025-03-22 20:37:58 -0400

    add rehypeSanitize, remarkFrontmatter, remarkGfm

--- services/markdown/src/render.ts
+++ services/markdown/src/render.ts
@@ -1,5 +1,8 @@
 import rehypeShikiFromHighlighter from '@shikijs/rehype/core';
+import rehypeSanitize from 'rehype-sanitize';
 import rehypeStringify from 'rehype-stringify';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
@@ -32,7 +35,10 @@ export async function render(raw: string, options?: RenderOptions): Promise<stri
 
 	const file = await unified()
 		.use(remarkParse)
+		.use(remarkFrontmatter)
+		.use(remarkGfm)
 		.use(remarkRehype)
+		.use(rehypeSanitize)
 		// @ts-expect-error - rehypeShikiFromHighlighter expects a HighlighterGeneric, but highlighter is a HighlighterCore
 		.use(rehypeShikiFromHighlighter, highlighter, highlighterOptions)
 		.use(rehypeStringify)
