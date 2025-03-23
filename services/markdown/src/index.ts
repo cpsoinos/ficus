86e18307588b747d7c357616f26090a140665428
Corey Psoinos <coreypsoinos@gmail.com>
2025-03-23 12:05:23 -0400

    set up sentry for markdown

--- services/markdown/src/index.ts
+++ services/markdown/src/index.ts
@@ -1,13 +1,24 @@
+import * as Sentry from '@sentry/cloudflare';
+
 import { BindingsSingleton } from './lib/bindings';
 import { render } from './render';
 
-export default {
-	async fetch(request, env, _ctx): Promise<Response> {
-		BindingsSingleton.initialize(env);
+const handler = Sentry.withSentry(
+	(_env) => ({
+		dsn: 'https://6f53f64e54966ce49795387f356e4bff@o4508996084039680.ingest.us.sentry.io/4509027808837632',
+		// Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
+		// Learn more at
+		// https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+		tracesSampleRate: 1.0
+	}),
+	{
+		async fetch(request, env, _ctx): Promise<Response> {
+			BindingsSingleton.initialize(env);
+			const body = await request.text();
+			const html = await render(body);
+			return new Response(html);
+		}
+	} satisfies ExportedHandler<Env>
+);
 
-		const body = await request.text();
-		const html = await render(body);
-
-		return new Response(html);
-	}
-} satisfies ExportedHandler<Env>;
+export default handler;
