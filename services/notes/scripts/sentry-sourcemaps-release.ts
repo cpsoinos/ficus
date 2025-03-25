cb3a3d7e02b34b6602cac76b71f42251ef1bd194
Corey Psoinos <coreypsoinos@gmail.com>
2025-03-25 19:43:24 -0400

    fix(notes): remove cli flags from sentry scripts, and use env vars instead

--- services/notes/scripts/sentry-sourcemaps-release.ts
+++ services/notes/scripts/sentry-sourcemaps-release.ts
@@ -1,9 +1,5 @@
 import { exec } from 'child_process';
 
-const authToken = process.env.SENTRY_AUTH_TOKEN;
-const projectName = process.env.SENTRY_PROJECT;
-const orgName = process.env.SENTRY_ORG;
-
 async function execAsync(command: string): Promise<string> {
 	return new Promise((resolve, reject) => {
 		exec(command, (err, stdout) => {
@@ -17,14 +13,10 @@ async function execAsync(command: string): Promise<string> {
 }
 
 // inject markers into sourcemaps
-await execAsync(
-	`pnpm sentry-cli sourcemaps inject --org ${orgName} --project ${projectName} ./dist`
-);
+await execAsync(`pnpm sentry-cli sourcemaps inject ./dist`);
 
 // upload sourcemaps to sentry
-await execAsync(
-	`pnpm sentry-cli sourcemaps upload --org ${orgName} --project ${projectName} --auth-token ${authToken} ./dist`
-);
+await execAsync(`pnpm sentry-cli sourcemaps upload ./dist`);
 
 // generate a release version
 const version = await execAsync('pnpm sentry-cli releases propose-version');
@@ -32,11 +24,7 @@ const version = await execAsync('pnpm sentry-cli releases propose-version');
 console.log(`Setting release version: ${version}`);
 
 // create a new release with sentry-cli
-await execAsync(
-	`pnpm sentry-cli --auth-token ${authToken} --org ${orgName} --project ${projectName} releases new ${version}`
-);
+await execAsync(`pnpm sentry-cli  releases new ${version}`);
 
 // set commits on the release
-await execAsync(
-	`pnpm sentry-cli --auth-token ${authToken} --org ${orgName} --project ${projectName} releases set-commits --auto ${version}`
-);
+await execAsync(`pnpm sentry-cli releases set-commits --auto ${version}`);
