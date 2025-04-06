31b595060199d294600fdb1d6b81b72ce7771d2f
Corey Psoinos <coreypsoinos@gmail.com>
2025-04-06 15:59:12 -0400

    configure turbo tasks

--- packages/rate-limiter/ambient.d.ts
+++ packages/rate-limiter/ambient.d.ts
@@ -1,5 +1,5 @@
 declare module 'cloudflare:test' {
 	// Controls the type of `import("cloudflare:test").env`
-	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+
 	interface ProvidedEnv extends Env {}
 }
